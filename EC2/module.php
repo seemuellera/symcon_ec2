cdc978b72fdb5a4af4c60390ceba21a052147477
Andreas Seemueller <andreas.seemueller@gmail.com>
2021-11-23 22:16:28 +0100

    test

--- EC2/module.php
+++ EC2/module.php
@@ -143,7 +143,7 @@ class EC2 extends IPSModule {
 			'InstanceIds' => [$this->ReadPropertyString('EC2InstanceId')]
 		]);
 		
-		var_dump($ec2InstanceStatusInformation->getPath('InstanceStatuses'));
+		var_dump($ec2InstanceStatusInformation->getPath('InstanceStatuses/0'));
 		
 		if ( count($ec2InstanceStatusInformation['InstanceStatuses']) == 0 ) {
 			
